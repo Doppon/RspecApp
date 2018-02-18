210d9d068e09eb5d8dccc324c233060eb9c3b546
Doppo Sekino <zaxpkuraudo7777@gmail.com>
2018-02-19 00:12:54 +0900

    レファクタリング

--- config/application.rb
+++ config/application.rb
@@ -16,15 +16,15 @@ module RspecApp
     # -- all .rb files in that directory are automatically loaded.
 
     # モデル、コントローラー、scaffoldを作成時にspecファイルを自動生成
-    config.generatorsdo|g|
+    config.generators do |g|
       g.test_framework :rspec,
-        fixtures: true,
-        view_specs: false,
-        helper_specs: false,
-        routing_specs: false,
-        controller_specs: true,
-        request_specs: false
-      .fixture_replacement :factory_girl, dir: "spec/factories"
+        fixtures, :true,
+        view_specs, :false,
+        helper_specs, :false,
+        routing_specs, :false,
+        controller_specs, :true,
+        request_specs, :false
+      g.fixture_replacement :factory_girl, dir: "spec/factories"
     end
   end
 end
